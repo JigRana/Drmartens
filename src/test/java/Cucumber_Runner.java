5c43746cb4fea0eef054f50e3a636829e3156f76
User <user@Users-MacBook-Air.local>
2021-11-03 23:01:41 +0000

    reports generate

--- src/test/java/Cucumber_Runner.java
+++ src/test/java/Cucumber_Runner.java
@@ -5,12 +5,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources",
         dryRun = false, plugin = {
-        "html:target/cucumber-html-report",
+
         "json:target/cucumber.json",
-        "pretty:target/cucumber-pretty.txt",
-        "usage:target/cucumber-usage.json",
-        "junit:target/cucumber-results.xml",
-        "progress:target/cucumber-progress.txt"
+        
 },
 
         tags = "@jr")
